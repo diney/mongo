77b8ca21fc5e7ac07a1195477fd6875032f5b381
Diney Souza <diney.dom@gmail.com>
2018-05-06 10:30:30 -0300

    Obtendo user por Id

---
+++ src/main/java/com/diney/workshopmongo/services/excepition/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.diney.workshopmongo.services.excepition;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
